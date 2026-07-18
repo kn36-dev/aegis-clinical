3c5c2fb49290ff0ac4b72e1cb9af41ff1877f1a6
Kean <keannenglim@gmail.com>
2026-07-18 22:31:24 +0800

    Removing dead implementations and verified architecture boundaries

--- src/aegis/database/__init__.py
+++ src/aegis/database/__init__.py
@@ -1,4 +1,3 @@
-from aegis.database.adapters import UpstashRedisAdapter, UpstashVectorAdapter
 from aegis.database.connection import get_db_connection
 from aegis.database.database import (
     DEFAULT_CLINICAL_DB_PATH,
@@ -8,20 +7,10 @@ from aegis.database.database import (
     init_clinical_database,
     init_graph_database,
 )
-from aegis.database.repository import (
-    ClinicalMatchRecord,
-    ClinicalRegistryRepository,
-    Icd11TaxonomyRecord,
-)
 
 __all__ = [
-    "ClinicalMatchRecord",
-    "ClinicalRegistryRepository",
     "DEFAULT_CLINICAL_DB_PATH",
     "DEFAULT_GRAPH_DB_PATH",
-    "Icd11TaxonomyRecord",
-    "UpstashRedisAdapter",
-    "UpstashVectorAdapter",
     "get_database_status",
     "get_db_connection",
     "init_all_databases",
